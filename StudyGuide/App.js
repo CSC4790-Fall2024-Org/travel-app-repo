8397b6f683e391e9d47a684c9cba417521d8eed1
ellaforese <ellaforese@gmail.com>
2024-09-12 11:33:01 -0400

    Connected with firestore

--- StudyGuide/App.js
+++ StudyGuide/App.js
@@ -1,6 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCz4zCemGt4XZKZCuCI_FwQwXSFxeaqvk0",
+  authDomain: "studyguide-ea1f4.firebaseapp.com",
+  projectId: "studyguide-ea1f4",
+  storageBucket: "studyguide-ea1f4.appspot.com",
+  messagingSenderId: "1090855415134",
+  appId: "1:1090855415134:web:e68c6916b6c5b7e5d9f3cf"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
 export default function App() {
   return (
     <View style={styles.container}>
