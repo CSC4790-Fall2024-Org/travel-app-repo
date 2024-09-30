b7b20ca1c94aecd7d9dc4a9f9ceb73f9d24d9eac
Madison Day <mday4@villanova.edu>
2024-09-30 11:26:21 -0400

    added useNavigation import to findfoodposts

--- StudyGuide/FindFoodPosts.js
+++ StudyGuide/FindFoodPosts.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Text, View, StyleSheet } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 
 /*export default function FindFoodPost() {
   return (
