87732d699e6405a15e2f3f792c62b734823b4572
MadisonDay <130935628+daym4@users.noreply.github.com>
2024-10-23 13:52:00 -0400

    worked on getting and filtering data from db for FindFoodPosts page

--- StudyGuide/FindFoodPosts.js
+++ StudyGuide/FindFoodPosts.js
@@ -1,13 +1,18 @@
 import { collection, getDocs } from "firebase/firestore";
 import React, { useState, useEffect } from "react";
 import { Text, View, StyleSheet, ScrollView, Button } from "react-native";
-import { useNavigation } from "@react-navigation/native";
+import { useNavigation, firestore } from "@react-navigation/native";
 import { db } from './firebase';
+//import firestore from '@react-native-firebase/firestore';
+//for new attempt to load data:
+//import { collection, query, where, getDocs } from "firebase/firestore";
 
   
   export default function FindFoodPosts() {
     const [locations, setLocations] = useState([]);
     const navigation = useNavigation();
+    const [sortedPosts, setSortedPosts]= useState([]);
+    const [showView, setShowView] = useState(false);
   
     // Function to fetch documents from Firestore
     const fetchLocations = async () => {
@@ -26,18 +31,69 @@ import { db } from './firebase';
     useEffect(() => {
       fetchLocations();
     }, []);
-  
-    const handleLocationPress = (locationId) => {
-      navigation.navigate("Posts"); // Navigate to posts page with location ID
-     /* const citiesRef = db.collection('cities');
 
+ /*   //two things happen on pressing a location
+  const handleLocationPress = (locationId) => {
+    navigateToPosts();
+    displayPosts();
+  };*/
+
+ 
+  
+    
+
+    const handleLocationPress = (locationId) => {
+      //call function
+      fetchSortedPosts(locationId);
+    };
+      // Function to fetch food posts (filtered) documents from Firestore
+    const fetchSortedPosts = async (locationId) => {
+      try {
+        const q = query(collection(db, "foodPosts"), where('locat_id', '==', locationId));
+        const snapshot = await getDocs(q);
+        const fetchedSortedPosts = [];
+        snapshot.forEach((doc) => {
+          fetchedSortedPosts.push({ id: doc.id, ...doc.data() });
+        });
+        setLocations(fetchedSortedPosts);
+      } catch (error) {
+        console.error("Error fetching posts for this location: ", error);
+      }
+    };
+  
+    useEffect(() => {
+      fetchSortedPosts();
+    }, []);
+  //};
+    
+
+/*most up to date!!!!!!
+const handleLocationPress = (locationId) => {
+  //db.collection
+const foodRef = firestore().collection('foodPosts');
+const snapshot = foodRef.where('locat_id', '==', locationId).get();
+if (snapshot.empty) {
+  console.log('Error: No matching Posts.');
+  return;
 }  
 
 snapshot.forEach(doc => {
   console.log(doc.id, '=>', doc.data());
-});*/
-    };
+});
+};//end of most up to date*/
+
+
+
+ 
+
+/*
+    const handleLocationPress = (locationId) => {
+      //this one line navigates you to Posts page:
+      navigation.navigate("Posts"); // Navigate to posts page with location ID
+     
+    };*/
   
+    /* orig return
     return (
       <View style={styles.container}>
         <Text style={styles.title}>Select Location</Text>
@@ -48,6 +104,35 @@ snapshot.forEach(doc => {
                 title={location.city} // Assuming each location document has a 'name' field
                 onPress={() => handleLocationPress(location.id)}
               />
+            </View>
+          ))}
+        </ScrollView>
+      </View>
+    );*/
+    return (
+      <View style={styles.container}>
+        <Text style={styles.title}>Select Location</Text>
+        <ScrollView>
+          {locations.map((location) => (
+            <View key={location.id} style={styles.locationContainer}>
+              <Button
+                title={location.city} // Assuming each location document has a 'name' field
+                //onPress={() => handleLocationPress(location.id), showView}
+                onPress={() => setShowView(true)}
+              />
+              {showView && (
+                <ScrollView>
+                  {sortedPosts.map((sortedPost) => (
+                  /*<View key={post_id} style={styles.postContainer}>
+                    <Button
+                    title={location.city} // Assuming each location document has a 'name' field
+                    onPress={() => handleLocationPress(location.id)}
+                     />
+                  </View>*/
+                    <View key={sortedPost.id} style={styles.postContainer}></View>
+                  ))}
+                </ScrollView>
+              )}
             </View>
           ))}
         </ScrollView>
@@ -67,6 +152,9 @@ snapshot.forEach(doc => {
       textAlign: "center",
     },
     locationContainer: {
+      marginBottom: 15,
+    },
+    postContainer: {
       marginBottom: 15,
     },
   });
--- StudyGuide/Posts.js
+++ StudyGuide/Posts.js
@@ -1,6 +1,24 @@
 import React from "react";
 import { Text, View, StyleSheet } from "react-native";
 
+
+/*most up to date!!!!!!
+const handleLocationPress = (locationId) => {
+  //db.collection
+const foodRef = firestore().collection('foodPosts');
+const snapshot = foodRef.where('locat_id', '==', locationId).get();
+if (snapshot.empty) {
+  console.log('Error: No matching Posts.');
+  return;
+}  
+
+snapshot.forEach(doc => {
+  console.log(doc.id, '=>', doc.data());
+});
+};//end of most up to date*/
+
+
+
 export default function Posts() {
   return (
     <View style={styles.container}>
