24f5b9c03c2607613bcab36636e0ced5733a4c15
MadisonDay <130935628+daym4@users.noreply.github.com>
2024-11-06 11:32:49 -0500

    "showing more fields in Posts, pulling locat inf"

--- StudyGuide/Posts.js
+++ StudyGuide/Posts.js
@@ -1,71 +1,18 @@
 import { Text, View, StyleSheet, ScrollView, Button } from "react-native";
-//import { Text, View, StyleSheet, ScrollView, Button } from "react-native";
 import { db } from './firebase';
-//import { collection, getDocs, query, where } from "firebase/firestore";
 import React, { useState, useEffect } from "react";
-//import { locationId, attribute } from './FindFoodPosts';
 import { getFirestore, firestore, where, collection, getDocs, query,  Filter, doc, QueryFieldFilterConstraint, DocumentSnapshot, QuerySnapshot } from "firebase/firestore";
-//import firestore from '@react-native-firebase/firestore';
-//for working code: 
 import { useNavigation } from "@react-navigation/native";
-
-
-//import {firestore, where, collection } from '@react-native-firebase/firestore';
-
 
 export default function Posts({ route }) { 
   const db = getFirestore(); // Firestore database instance
   const navigation = useNavigation();
   const [sortedPosts, setSortedPosts]= useState([]);
-  //const [showView, setShowView] = useState(false);
   const { location_id }=route.params;
- // const {foodcity}=route.params;
-
- /*
-useEffect(()=> {
-  const postSnap = firestore().collection('foodPosts')
-  .where('locat_id', '==', location_id)
-  onSnapshot(querySnapshot => {
-    const sortedPostData = [];
-    querySnapshot.forEach(doc => {
-      sortedPostData.push({ id: doc.id, ...doc.data() });
-    });
-    setSortedPosts;
-
-});
-return ()=> postSnap;
-}, []);
- */
+  const [locatInfo, setlocatInfo]= useState([]);
 
 
-  //has bug
- 
-  
-// const fetchSortedPosts = async () => {
-//     try { 
-
-//      //works but it lists all posts
-//      console.log(location_id);
-//      const querySnapshot = await getDocs(collection(db, "foodPosts"), where('locat_id', '==', location_id));
-//     // const querySnapshot = await getDocs(collection(db, "foodPosts"), where('locat_id', 'in', [{location_id}]));
-//    // const querySnapshot= db.where('locat_id', '==', 'location_id');
-//    //const querySnapshot = firestore().collection('foodPosts').where('locat_id', '==', location_id).get();
-     
-//      //where to push docs with locat_id
-//       const fetchedSortedPosts = [];
-//       //pushing on fetched posts
-//       querySnapshot.forEach((doc) => {
-//         // fetchedSortedPosts.push({ id: doc.id, ...doc.data() });
-        
-//         fetchedSortedPosts.push({ id: doc.id, ...doc.data() }); 
-//       });
-//       setSortedPosts(fetchedSortedPosts);//setting what gets displayed
-      
-//     } catch (error) {
-//       console.error("Error fetching posts for this location: ", error);
-//     } //end of works
-      
-//   }; //end of const 
+ //get the Food Posts for the location chosen in FindFoodPosts
 const fetchSortedPosts = async () => {
   try {
     console.log(location_id);
@@ -81,44 +28,79 @@ const fetchSortedPosts = async () => {
     querySnapshot.forEach((doc) => {
       fetchedSortedPosts.push({ id: doc.id, ...doc.data() });
     });
-
     setSortedPosts(fetchedSortedPosts); // Update state with the filtered posts
   } catch (error) {
     console.error("Error fetching posts for this location: ", error);
   }
 };
-
-
 useEffect(() => {
   fetchSortedPosts();
 }, [db, 'foodPosts']);
-
-
 if (!sortedPosts) {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Loading...</Text>
     </View>
   );
+}// end of getting the Food Posts for the location chosen in FindFoodPosts
 
+//get Location using location Id passed into this page
+const fetchLocatInfo = async () => {
+  try {
+    console.log(location_id);
 
-}
+    // Build the query with the collection and where filter
+    const locatRef = collection(db, "locations");
+    const q = query(foodPostsRef, where('locat_id', '==', location_id));
 
+    // Execute the query and get the documents
+    const querySnapshot = await getDocs(q);
 
+    const fetchedLocatInfo = [];
+    querySnapshot.forEach((doc) => {
+      fetchedLocatInfo.push({ id: doc.id, ...doc.data() });
+    });
+    setlocatInfo(fetchedLocatInfo); // Update state with the filtered posts
+  } catch (error) {
+    console.error("Error fetching info for this location: ", error);
+  }
+};
+useEffect(() => {
+  fetchSortedPosts();
+}, [db, 'locations']);
+
+//come back to userId field that has been taken out of foodPosts fields
+//also need to add addr (address), userId, food_city, link
+//make sure the field names match in here and create food posts so that 
 
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Posts with ID {location_id}  </Text>
     <ScrollView>
+    {locatInfo.map((location) => (
+        <View key={location.id} style={styles.container}>
+          <Button
+            title={location.city} // Assuming each location document has a 'name' field
+            onPress={() => handleLocationPress(location.id)}
+          />
+          <Text style={styles.itemTitle}> Locat City: <Text style={styles.postItem}>{locatInfo.city}</Text></Text>
+          <Text style={styles.itemTitle}> Locat Country: <Text style={styles.postItem}>{locatInfo.country}</Text></Text>
+        </View>
+      ))}
+
       {sortedPosts.map((sortedPost) => (
        
             <View key={sortedPost.id} style={styles.container}>
-            <Text style={styles.itemTitle}> Post from <Text style={styles.postItem}>{sortedPost.userId}</Text></Text>
+            <Text style={styles.itemTitle}> Post from: <Text style={styles.postItem}>{sortedPost.userId}</Text></Text>
             <Text style={styles.itemTitle}> Restaurant Name: <Text style={styles.postItem}>{sortedPost.restaurant}</Text></Text>
             <Text style={styles.itemTitle}> Restaurant City: <Text style={styles.postItem}>{sortedPost.food_city}</Text></Text>
-            <Text style={styles.itemTitle}> Message: <Text style={styles.postItem}>{sortedPost.message}</Text></Text>
+            <Text style={styles.itemTitle}> Expense: <Text style={styles.postItem}>{sortedPost.expense}</Text></Text>
+            <Text style={styles.itemTitle}> Meal Time: <Text style={styles.postItem}>{sortedPost.mealTime}</Text></Text>
+            <Text style={styles.itemTitle}> Restaurant Type: <Text style={styles.postItem}>{sortedPost.restaurantType}</Text></Text>
             <Text style={styles.itemTitle}> Location Id: <Text style={styles.postItem}>{sortedPost.locat_id}</Text></Text>
-
+            <Text style={styles.itemTitle}> Address: <Text style={styles.postItem}>{sortedPost.addr}</Text></Text>
+            <Text style={styles.itemTitle}> Link to website: <Text style={styles.postItem}>{sortedPost.link}</Text></Text>
+            <Text style={styles.itemTitle}> Description/Message: <Text style={styles.postItem}>{sortedPost.description}</Text></Text>
         </View>
 
           
