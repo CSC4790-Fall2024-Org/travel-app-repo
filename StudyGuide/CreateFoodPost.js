54d9e44373b9bfae3d711908a6206715b5fd2961
MadisonDay <130935628+daym4@users.noreply.github.com>
2024-11-21 12:18:06 -0500

    fixed navigation to FoodPosts from creating

--- StudyGuide/CreateFoodPost.js
+++ StudyGuide/CreateFoodPost.js
@@ -147,7 +147,7 @@ const CreateFoodPost = () => {
         await setDoc(newPostRef, newPostData);
   
         // Navigate to the "FindFoodPosts" screen with the location_id
-        navigation.navigate('Posts', { location_id: restaurantLocationId });
+        navigation.navigate('FoodPosts', { location_id: restaurantLocationId });
         // don't let them navigate backwards to create food post again
   
         console.log("New post added successfully!");
